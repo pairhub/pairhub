61c0cbbf0700ee141a294a6e1b0fbee6bad6e3bd
Gustav Larsson <gustav.larsson@gmail.com>
2017-04-15 17:04:09 +0200

    Clean up onCreateUser

--- server/accounts.js
+++ server/accounts.js
@@ -1,13 +1,9 @@
 Accounts.onCreateUser((options, user) => {
-    var accessToken = user.services.github.accessToken,
-        result,
-        profile;
+    const accessToken = user.services.github.accessToken;
 
-    result = Meteor.http.get('https://api.github.com/user', {
-      params : {
-          access_token : accessToken
-      },
-      headers: {"User-Agent": "Meteor/1.0"}
+    const result = Meteor.http.get('https://api.github.com/user', {
+      params: { access_token: accessToken },
+      headers: { "User-Agent": "Meteor/1.0" }
     });
 
     if (result.error) {
@@ -15,16 +11,16 @@ Accounts.onCreateUser((options, user) => {
         throw result.error;
     }
 
-    profile = _.pick(result.data,
-      'name',
-      'avatar_url',
-      'company',
-      'blog',
-      'location',
-      'bio',
-      'html_url'
-    );
-    
+    const profile = {
+      'name': result.data.name,
+      'avatar_url': result.data.avatar_url,
+      'company': result.data.company,
+      'blog': result.data.blog,
+      'location': result.data.location,
+      'bio': result.data.bio,
+      'html_url': result.data.html_url
+    }
+
     user.profile = profile;
 
     return user;
