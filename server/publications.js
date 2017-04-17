99badddd0b4e750a31de37b7f4f0b64a2f5979be
Gustav Larsson <gustav.larsson@gmail.com>
2017-04-17 16:23:45 +0200

    Remove arrow function from publication

--- server/publications.js
+++ server/publications.js
@@ -1,11 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.publish('userData', () => {
+Meteor.publish('userData', function() {
   return Meteor.users.find({_id: this.userId},
     {fields: {'services': 1}});
 });
 
-Meteor.publish('landingUsers', () => {
+Meteor.publish('landingUsers', function() {
   return Meteor.users.find({}, {
     fields: {
       'profile.name': 1,
