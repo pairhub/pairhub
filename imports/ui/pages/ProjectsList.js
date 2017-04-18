f4a5d02fbda06157b276d9e1f52b89f25ea276b0
Gustav Larsson <gustav.larsson@gmail.com>
2017-04-18 19:20:14 +0200

    Break out add project

--- imports/ui/pages/ProjectsList.js
+++ imports/ui/pages/ProjectsList.js
@@ -3,33 +3,19 @@ import { createContainer } from 'meteor/react-meteor-data';
 import Projects from '../../api/projects';
 import { Row, Col, Card, CardBlock, CardTitle, CardText, CardSubtitle } from 'reactstrap';
 
-//TODO: separate out create new project into new component
+import AddProject from '../components/AddProject';
+
 class ProjectsList extends Component {
-  addProject(event) {
-    event.preventDefault();
-    const title = this.refs.title.value.trim();
-    const desc = this.refs.desc.value.trim();
-    if(title !== '' && desc !== '') {
-      console.log('hello');
-      Meteor.call('createNewProject', title, desc, (err, res) => {
-        console.log('hullo');
-        console.log(err);
-        if (!err) {
-          this.refs.title.value = '';
-          this.refs.desc.value = '';
-        }
-      })
-    }
-  }
+
   render() {
     return (
       <div>
-        <div>Projects page! Woop!</div>
-        <form onSubmit={this.addProject.bind(this)}>
+        <AddProject />
+        {/* <form onSubmit={this.addProject.bind(this)}>
           <input type="text" ref="title" />
           <input type="text" ref="desc" />
           <button type="submit">Create project</button>
-        </form>
+        </form> */}
         {this.props.projects.map((project) => {
           return (
             <Card key={project._id}>
