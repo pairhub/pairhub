785e489ba5badcd7091a9f9ae53fb4b203e143d7
Gustav Larsson <gustav.larsson@gmail.com>
2017-05-01 21:10:07 +0200

    Add login button

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,28 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import { Navbar, Container, NavbarBrand, Nav, NavItem } from 'reactstrap'
+import { Navbar, Container, NavbarBrand, Nav, NavItem, Button } from 'reactstrap'
 
 const Header = () => (
   <div>
     <Head>
       <title>PairHub</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="Find remote pair programming buddies" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
+      <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32" />
+      <script src="https://use.fontawesome.com/49ef5fd256.js"></script>
+      <style>{`
+        .btn-secondary {
+          color: #0000ff;
+          border-color: #0000ff;
+          cursor: pointer;
+        }
+        .btn-secondary:hover {
+          color: white;
+          border-color: #0000ff;
+          background-color: #0000ff;
+        }
+      `}</style>
     </Head>
 
     <Navbar color="faded" light toggleable style={{padding: '0px', marginBottom: '50px'}}>
@@ -22,6 +37,7 @@ const Header = () => (
         </Nav>
         <Nav navbar className="ml-auto">
           {/* {this.props.authenticated ? <NavAuthenticatedUser /> : <NavLoginButton />} */}
+          <NavItem><Button><i className="fa fa-github" aria-hidden="true"></i> Log in with GitHub</Button></NavItem>
 
         </Nav>
       </Container>
