660cdabb7f107b87f866166768037a620835e23d
Gustav Larsson <gustav.larsson@gmail.com>
2017-05-01 17:25:21 +0200

    Remove Meteor

--- imports/ui/pages/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const About = () => (
-  <div>
-    <h1>About us!</h1>
-    <p>Hello!</p>
-  </div>
-)
-export default About;
--- server/main.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-import Projects from '../imports/api/projects';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
