018cf533fa0915074abe919661995bb504bdc41e
Gustav Larsson <gustav.larsson@gmail.com>
2017-05-11 04:44:50 +0200

    Update about

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,8 @@ import Layout from '../components/Layout'
 
 export default () => (
   <Layout>
-    <p>This is the about page.</p>
+    <h2>Hello there 👋</h2>
+    <p>This the very early beta of PairHub, a remote pair programing web app and community. PairHub will help you log and track pair programming sessions and help you find pair programming buddies from all over the world!</p>
+    <p>The project is open source, and being developed by <a href="https://twitter.com/gustavlrsn" target="_blank">@gustavlrsn</a>. Join the development and share your ideas <a href="https://github.com/pairhub/pairhub" target="_blank">on GitHub</a>! 🙏</p>
   </Layout>
 )
