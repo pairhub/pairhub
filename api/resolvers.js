e6b3447667aac9062f9794c9a00f1053f572b498
Gustav Larsson <gustav.larsson@gmail.com>
2018-03-17 14:26:29 +0100

    Simplify return statements of resolvers

--- api/resolvers.js
+++ api/resolvers.js
@@ -3,46 +3,19 @@ export default {
     email: (parent, args, { currentUser }) => (currentUser ? parent.email : null),
   },
   Post: {
-    comments: async (parent, args, { Comment }) => {
-      const comments = await Comment.find({ postId: parent._id });
-      return comments;
-    },
-    author: async (parent, args, { User }) => {
-      const user = await User.findOne({ userId: parent.authorId });
-      return user;
-    },
+    comments: async (parent, args, { Comment }) => Comment.find({ postId: parent._id }),
+    author: async (parent, args, { User }) => User.findOne({ userId: parent.authorId }),
   },
   Comment: {
-    post: async (parent, args, { Post }) => {
-      const post = await Post.findOne({ _id: parent.postId });
-      return post;
-    },
-    author: async (parent, args, { User }) => {
-      const user = await User.findOne({ userId: parent.authorId });
-      return user;
-    },
+    post: async (parent, args, { Post }) => Post.findOne({ _id: parent.postId }),
+    author: async (parent, args, { User }) => User.findOne({ userId: parent.authorId }),
   },
   Query: {
-    user: async (_, { id }, { User }) => {
-      const user = await User.findOne({ userId: id });
-      return user;
-    },
-    allUsers: async (_, args, { User }) => {
-      const users = await User.find();
-      return users;
-    },
-    post: async (_, { id }, { Post }) => {
-      const post = await Post.findOne({ _id: id });
-      return post;
-    },
-    allPosts: async (_, args, { Post }) => {
-      const posts = await Post.find();
-      return posts;
-    },
-    comment: async (_, { id }, { Comment }) => {
-      const comment = await Comment.findOne({ _id: id });
-      return comment;
-    },
+    user: async (_, { id }, { User }) => User.findOne({ userId: id }),
+    allUsers: async (_, args, { User }) => User.find(),
+    post: async (_, { id }, { Post }) => Post.findOne({ _id: id }),
+    allPosts: async (_, args, { Post }) => Post.find(),
+    comment: async (_, { id }, { Comment }) => Comment.findOne({ _id: id }),
   },
   Mutation: {
     createPost: async (_, { title, content }, { Post, currentUser }) => {
