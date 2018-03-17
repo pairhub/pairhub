c42f2ced977742c3357e34760454fe48537d8f76
Gustav Larsson <gustav.larsson@gmail.com>
2018-03-17 15:19:30 +0100

    Change port to 3010

--- api/index.js
+++ api/index.js
@@ -11,34 +11,30 @@ import setupGitHubLogin from './auth';
 import { User, Post, Comment } from './models';
 
 dotenv.config();
+mongoose.connect('mongodb://localhost/db');
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
-mongoose.connect('mongodb://localhost/db');
-
-const PORT = 3000;
-
 const app = express();
+const PORT = 3010;
 
 setupGitHubLogin(app);
 
 app.use(
   '/graphql',
   bodyParser.json(),
-  graphqlExpress(req =>
-    // Query too large check here?
-    ({
-      schema,
-      context: {
-        currentUser: req.user,
-        User,
-        Post,
-        Comment,
-      },
-    })),
+  graphqlExpress(req => ({
+    schema,
+    context: {
+      currentUser: req.user,
+      User,
+      Post,
+      Comment,
+    },
+  })),
 );
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
