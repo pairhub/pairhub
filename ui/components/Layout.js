f76a0d2682fcf9e97d99e94a9645dc7a1a9c1df9
Gustav Larsson <gustav.larsson@gmail.com>
2018-03-21 08:13:39 +0100

    Add basic styles

--- ui/components/Layout.js
+++ ui/components/Layout.js
@@ -1,15 +1,37 @@
 import Header from "./Header";
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: "1px solid #DDD"
-};
-
 const Layout = props => (
-  <div style={layoutStyle}>
-    <Header />
-    {props.children}
+  <div className="layout">
+    <div className="header">
+      <Header />
+    </div>
+    <div className="content">{props.children}</div>
+
+    <style jsx>{`
+      .layout {
+        display: grid;
+        grid-template-rows: 60px 1fr;
+      }
+      .header {
+        background: #f4f5f6;
+        display: grid;
+        align-items: center;
+        padding: 20px;
+      }
+      .content {
+        padding: 20px;
+      }
+    `}</style>
+
+    <style jsx global>{`
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+          Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
+          "Segoe UI Symbol";
+      }
+    `}</style>
   </div>
 );
 
