0c8219bd688c1bb3c5a7d7f4e1e1611bc2b75b69
Gustav Larsson <gustav.larsson@gmail.com>
2018-04-19 08:41:14 +0200

    Add sidebar

--- ui/pages/index.js
+++ ui/pages/index.js
@@ -1,10 +1,43 @@
+import styled from "styled-components";
+
 import Layout from "../components/Layout";
 import withData from "../lib/withData";
 import Posts from "../components/Posts";
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 5fr 2fr;
+  grid-gap: 20px;
+`;
+
+const Button = styled.button`
+  background: #0000ff;
+  width: 100%;
+  display: block;
+  color: white;
+  font-size: 14px;
+  padding: 18px;
+  border-radius: 6px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  border: 0;
+  cursor: pointer;
+
+  &:hover {
+    background: #0000af;
+  }
+`;
+
+const Sidebar = styled.div``;
+
 const Index = () => (
   <Layout>
-    <Posts />
+    <Grid>
+      <Posts />
+      <Sidebar>
+        <Button>New post</Button>
+      </Sidebar>
+    </Grid>
   </Layout>
 );
 
