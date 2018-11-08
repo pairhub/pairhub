e35162488fcfd91c380e63ae40503cffe28962e1
Gustav Larsson <gustav.larsson@gmail.com>
2018-11-08 22:54:54 +0100

    Update wording in meta description

--- ui/components/Head.js
+++ ui/components/Head.js
@@ -4,7 +4,7 @@ export default () => (
   <Head>
     <title>PairHub</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <meta name="description" content="Find remote pair programming buddies" />
+    <meta name="description" content="Find remote pair programming partners" />
     <link
       rel="icon"
       type="image/png"
